609d3ba5342c8ae8eb03cca5c9ef55a806f919c7
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2023-05-13 23:17:31 +0200

    update

--- Clicker/script.js
+++ Clicker/script.js
@@ -1,28 +1,52 @@
-{
+//{
 
+	let MAIN_URL = 'http://127.0.0.1:5500/clicker'
+	let SHOP_URL = 'http://127.0.0.1:5500/shop'
+	
+	if (window.location == MAIN_URL) {
+		let clickItem = document.getElementById('clicker-image')
+		let clickText = document.getElementById('coin-display')
+	}
 
-let clickItem = document.getElementById('clicker-image')
-let clickText = document.getElementById('coin-display')
-let coins = 0
+	let coins = 0
+	
+	// Load Data
+	let data = JSON.parse(localStorage.getItem("clickerData"))
+	
+	if (data != null) {
+		load()
+	} else {
+		data =
+		{
+			"coins": 0
+		}
+	}
+	
+	if (window.location == MAIN_URL) {
+		clickText.innerHTML = '<i class="material-icons" id="coin-image">attach_money</i>'+coins
+		clickItem.addEventListener('click',()=>{clickText.innerHTML = '<i class="material-icons" id="coin-image">attach_money</i>'+(++coins)})
+	}
 
-if (localStorage.getItem("coins")!=null) {
-	load()
-}
-
-clickText.innerHTML = '<i class="material-icons" id="coin-image">attach_money</i>'+coins
-
-clickItem.addEventListener('click',()=>{clickText.innerHTML = '<i class="material-icons" id="coin-image">attach_money</i>'+(++coins)})
-
-function save() {
-	localStorage.setItem("coins",coins)
-}
-
-function load() {
-	coins = localStorage.getItem("coins")
-}
-
-window.onbeforeunload = function (e) {
-	save()
-}
-
-}
+	function save() {
+		localStorage.setItem("clickerData",JSON.stringify({
+			"coins": coins
+		}));
+	}
+	
+	function load() {
+		coins = data.coins
+	}
+	
+	window.onbeforeunload = function (e) {
+		save()
+	}
+	
+	document.getElementById('navmain').addEventListener("click",() => {
+		window.location = MAIN_URL
+	})
+	
+	document.getElementById('shop').addEventListener("click",() => {
+		window.location = SHOP_URL
+	})
+	
+//}
